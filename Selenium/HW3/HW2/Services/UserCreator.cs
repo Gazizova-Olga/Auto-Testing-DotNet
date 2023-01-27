89de25d668fe3f0763829629df175548600a0279
GazOR <gazizova.or@gmail.com>
2023-01-28 00:37:11 +0300

    Second project copy

---
+++ Selenium/HW3/HW2/Services/UserCreator.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic.Models;
+
+namespace BusinessLogic.Services
+{
+    public static class UserCreator
+    {
+        private const string login = "Roman";
+        private const string password = "Jdi1234";
+
+        public static User GetUser()
+        {
+            return new User(login, password);
+        }
+    }
+}
