51a88f4c78afd285739750b2f3d2ff5f6af0aec1
GazOR <gazizova.or@gmail.com>
2023-02-13 11:22:43 +0300

    new item

---
+++ JDI/JDI_HW7/HW_7/HW_7/Project/Site/Custom/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using JDI_Web.Selenium.Elements.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_7.Project.Site.Custom
+{
+    public class MenuItem : WebElement
+    {
+        public MenuItem(By byLocator = null)
+            : base(byLocator)
+        {
+        }
+
+        public bool IsSelected()
+        {
+            return //HasClass("active") &&
+                 GetAttribute("ui").Equals("label");
+        }
+    }
+}
